ecbd6639cd18dad39a3d3f56b75a0c8dac8c8929
chamarez <michel.chamarez@gmail.com>
2021-12-08 02:03:47 +0100

    step 4

--- src/index.js
+++ src/index.js
@@ -1,48 +1,61 @@
-const firstExample =
-  "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-const secondExample =
-  "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.";
-const thirdExample = "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n14:27:00 Customer : Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra. \n14:27:47 Agent : Vestibulum tempor diam eu leo molestie eleifend. \n14:28:28 Customer : Contrary to popular belief, Lorem Ipsum is not simply random text."
+const firstExample ="14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+const secondExample ="14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.";
+const thirdExample ="14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n14:27:00 Customer : Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra. \n14:27:47 Agent : Vestibulum tempor diam eu leo molestie eleifend. \n14:28:28 Customer : Contrary to popular belief, Lorem Ipsum is not simply random text.";
+const fourExample ="14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.";
+const fiveExample = "14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.14:26:15 Agent : I received it at 12:24:48, ut blandit lectus."
 ///`hh:mm:ss`, `customer/agent name`, `:` and `sentence`
 const dateReg = /(\d{2}:\d{2}:\d{2})/;
 const mentionReg = /(.+\s:\s)/;
 const sentenceReg = /(.+):(.+)/;
 const typesReg = /\s.+\s:/;
-const allReg =  /(?<date>\d{2}:\d{2}:\d{2}) (?<person>.+) : (?<text>.+)$/
-
-
+const allReg = /(?<date>\d{2}:\d{2}:\d{2}) (?<person>.+) : (?<text>.+)$/;
+const dotdateReg = /(.+\.)(\d.+)/;
 
 const textIntoObject = (string) => {
   mention = string.match(mentionReg);
-  allr = string.match(allReg)
-  const spliter = string.split(": ");
-  const type = spliter[0].substring(9, spliter[0].length - 1);
-  console.log(string)
+  allr = string.match(allReg);
+  /*   const spliter = string.split(": ");
+  const type = spliter[0].substring(9, spliter[0].length - 1); */
+
   const result = {
     date: allr.groups.date,
     mention: mention[0],
     sentence: allr.groups.text,
     type: allr.groups.person.toLowerCase(),
   };
-    return [result]
-
+  return [result];
 };
 
 const multipleTextIntoObject = (string) => {
+  if (string.match(dotdateReg)) {
+    let stringDateSplitting = "";
+    const stringArray = string.match(dotdateReg);
+    for (let index = 1; index < stringArray.length; index++) {
+      if(index==1){
+        stringDateSplitting =`${stringArray[index]}`;
+      }else{
+        stringDateSplitting = `${stringDateSplitting}\n${stringArray[index]}`;
+
+      }
+    }
+
+    string = stringDateSplitting;
+  }
+  console.log(string);
   const spliterMesagges = string.split("\n");
+  console.log(spliterMesagges);
   let mesaggesObjets = [];
   for (let index = 0; index < spliterMesagges.length; index++) {
     mesaggesObjets[index] = textIntoObject(spliterMesagges[index]);
   }
   let mesaggesObjetsArray = [];
-  for (let index = 0; index < mesaggesObjets.length; index++){
-    mesaggesObjetsArray.push(mesaggesObjets[index][0])
+  for (let index = 0; index < mesaggesObjets.length; index++) {
+    mesaggesObjetsArray.push(mesaggesObjets[index][0]);
   }
+
   return mesaggesObjetsArray;
 };
 
-
-console.log(multipleTextIntoObject(firstExample))
+console.log(multipleTextIntoObject(fourExample));
 
 module.exports = { textIntoObject, multipleTextIntoObject };
-
